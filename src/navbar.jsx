b913fdf7af3267838077a88e9628b1e9c45ccf38
abrarfuad27 <abrarfuad51@gmail.com>
2023-07-30 17:54:30 +0600

    intialized starter files

---
+++ src/navbar.jsx
@@ -0,0 +1,34 @@
+import React from "react";
+
+export default function NavBar() {
+  return (
+    <>
+      <div className="intro">
+        <h4>Welcome to PomoDragon- a fast and simple Pomodoro tool</h4>
+      </div>
+      <nav className="nav-bar">
+        <img src="../logo.png" />
+        <ul className="nav-items">
+          <li className="nav-item">
+            <a
+              href="https://en.wikipedia.org/wiki/Pomodoro_Technique"
+              target="_blank"
+            >
+              Pomodoro
+            </a>
+          </li>
+          <li className="nav-item">
+            <a href="https://abrarfuad.vercel.app/" target="_blank">
+              About
+            </a>
+          </li>
+          <li className="nav-item">
+            <a href="" target="_blank">
+              Login
+            </a>
+          </li>
+        </ul>
+      </nav>
+    </>
+  );
+}
