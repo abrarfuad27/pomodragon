262f7212ca49dbe5b980dd86a6dceabc59570519
abrarfuad27 <abrarfuad51@gmail.com>
2023-08-06 16:46:11 +0600

    cors

--- api/index.js
+++ api/index.js
@@ -9,24 +9,7 @@ const cookieParser = require("cookie-parser");
 const dotenv = require("dotenv");
 dotenv.config();
 
-// Define the update operation with $set to add the new fields with default values
-const allowedOrigins = [
-  "http://localhost:3000",
-  "https://pomodragon.vercel.app",
-];
-
-app.use(
-  cors({
-    credentials: true,
-    origin: function (origin, callback) {
-      if (!origin || allowedOrigins.includes(origin)) {
-        callback(null, true);
-      } else {
-        callback(new Error("Not allowed by CORS"));
-      }
-    },
-  })
-);
+app.use(cors());
 app.use(express.json());
 app.use(cookieParser());
 
