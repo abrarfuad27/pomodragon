f5264b7353473992bc33944a62d155bc996c8d77
abrarfuad27 <abrarfuad51@gmail.com>
2023-12-11 21:47:10 -0500

    updated quotes

--- src/data.js
+++ src/data.js
@@ -7,17 +7,8 @@ export const quotes = [
   "Succeeding requires iterations. Remember, Rome wasn't built in a day.",
   "You don't have to see the whole staircase, just take the first step.",
   "Don't wait. The time will never be just right.",
-  "If at first, you don't succeed, skydiving is not for you.",
   "Feeling lost? Take a deep breath.",
-  "Random quote generator go brr.",
-  "Don't look back. March forward",
-  "Being a functional adult everyday seems a bit -excessive-.",
-  "Go easy on yourself. Whatever you do today, let it be enough",
-  "Listen to your gut feelings.",
-  "You can",
   "Life is not a race. It's a marathon. Pace yourself accordingly.",
-  "Love, laugh, live.",
   "Sleeping on my keyboard. If I answer, I am talking in my sleep. Goodnight.",
-  "Six month vacations twice a year.",
   // Add more motivational quotes as needed
 ];
